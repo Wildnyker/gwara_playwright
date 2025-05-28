e223e83f683219d2c419f096ad6c5d29cdc9ed10
Wildnyker <sgrynchuk@outlook.com>
2025-05-28 23:38:24 +0200

    created basic valid add post tests + started refactor to POM

--- pageModels/feedPage.ts
+++ pageModels/feedPage.ts
@@ -1,8 +1,33 @@
 import {expect,type Locator,type Page} from '@playwright/test'
 
 export class FeedPage{
-    readonly page: Page
+    readonly page: Page;
+    readonly titleField: Locator;
+    readonly imageField:Locator;
+    readonly postTextField: Locator;
+    readonly shareButton: Locator;
     constructor(page:Page){
         this.page = page
+        this.titleField = page.getByRole('textbox', { name: 'Чим хочеш поділитися?' })
+        this.imageField = page.getByPlaceholder('URL зображення')
+        this.postTextField = page.locator('#new-entry-text')
+        this.shareButton = page.getByRole('button', { name: 'Поділитися' })
     }
+
+    async addPost(title?:string, imageLink?:string, postText?:string){
+        if(title){
+            await this.titleField.fill(title);
+        }
+ 
+        if(imageLink){
+            await this.imageField.fill(imageLink);
+        }
+        
+        if(postText){
+            await this.postTextField.fill(postText);
+        }
+
+        await this.shareButton.click()
+    }
+
 }
--- tests/addingPosts.spec.ts
+++ tests/addingPosts.spec.ts
@@ -1,4 +1,6 @@
 import {test, expect} from '@playwright/test'
+import { FeedPage } from '../pageModels/feedPage';
+import { title } from 'process';
 
 test.beforeEach(async({page})=>{
     await page.goto('/')
@@ -7,7 +9,27 @@ test.beforeEach(async({page})=>{
 
 
 test('Add a valid post with title only', async({page})=>{
-    await page.getByRole('textbox', { name: 'Чим хочеш поділитися?' }).fill('Test name');
-    await page.getByRole('button', { name: 'Поділитися' }).click();
+    const feedPage = new FeedPage(page);
+    await feedPage.addPost("Test title 1")
+    await expect(page.locator('.entry-title')).toHaveText('Test title 1')
+    await expect(page.getByRole('button', { name: 'Висловитись' })).toBeVisible();
+})
+
+test('Add a valid post with title + body', async({page})=>{
+    const feedPage = new FeedPage(page);
+    await feedPage.addPost("Test title 2", undefined, "Test Body")
+    await expect(page.getByRole('button', { name: 'Висловитись' })).toBeVisible();
+    await expect(page.locator('.entry-title')).toHaveText('Test title 2')
+    await expect(page.getByText('Test Body 1')).toBeVisible()
+})
+
+
+test('Add a valid post with title + body + image', async({page})=>{
+    const feedPage = new FeedPage(page);
+    await feedPage.addPost("Test title 3", "https://clipart-library.com/data_images/320464.png","Test Body 2")
+    await expect(page.locator('.entry-title')).toHaveText('Test title 3')
+    await expect(page.locator('.entry-image')).toBeVisible()
+    await expect(page.locator('.entry-image')).toHaveAttribute('src', 'https://clipart-library.com/data_images/320464.png');
+    await expect(page.getByText('Test Body 2')).toBeVisible()
     await expect(page.getByRole('button', { name: 'Висловитись' })).toBeVisible();
 })
