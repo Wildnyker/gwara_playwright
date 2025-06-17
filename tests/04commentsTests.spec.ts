b77dcbc45c906a6ec8159429bbdb8379ed690a07
wildnyker <sgrynchuk@outlook.com>
2025-06-17 21:52:26 +0200

    new comments tests

--- tests/04commentsTests.spec.ts
+++ tests/04commentsTests.spec.ts
@@ -1,13 +1,12 @@
 import { test, expect } from '@playwright/test';
 import { PageManager } from '../pageModels/pageManager';
-import { TEST_COMMENT1, TESTUSER_1_NAME, TESTUSER_2_NAME } from './test data/testData';
+import { TEST_COMMENT1, TESTUSER_1_NAME, TESTUSER_2_NAME, MAX_LENGTH_COMMENT } from './test data/testData';
 
 test.use({ storageState: './.auth/user1.json' });
 
 test.beforeEach(async ({ page }) => {
     const pm = new PageManager(page);
     await page.goto('/');
-    await page.waitForTimeout(5000);
     await expect(pm.onFeedPage().openPostFormButton).toBeVisible();
 });
 
@@ -29,16 +28,36 @@ test('add valid comment to other user post', async ({ page }) => {
     await expect(pm.onPostPage().postCommentCounter).toHaveText('= Думки 1 = ');
 });
 
-// Future test cases (uncomment and implement as needed)
 
-// test('add empty comment', async ({ page }) => {
-// });
+test('add empty comment', async({page})=>{
+    const pm = new PageManager(page)
+    await pm.onFeedPage().postFeedTitle("Post title 2").click()
+    await expect(pm.onPostPage().postAuthor).toHaveText(TESTUSER_2_NAME)
+    await pm.onPostPage().addComment('')
+    await expect(pm.onPostPage().firstCommentBody).toContainText(TEST_COMMENT1)
+    await expect(pm.onPostPage().postCommentCounter).toHaveText('= Думки 1 = ')
+})
 
-// test('add max-length comment', async ({ page }) => {
-// });
+test('add max-length comment', async({page})=>{
+    const pm = new PageManager(page)
+    await pm.onFeedPage().postFeedTitle("Post title 2").click()
+    await expect(pm.onPostPage().postAuthor).toHaveText(TESTUSER_2_NAME)
+    await pm.onPostPage().addComment(MAX_LENGTH_COMMENT)
+    await expect(pm.onPostPage().firstCommentBody).toContainText(MAX_LENGTH_COMMENT)
+    await expect(pm.onPostPage().postCommentCounter).toHaveText('= Думки 2 = ') 
+})
 
-// test('check comment posting is not possible for not logged user', async ({ page }) => {
-// });
+test('check comment posting is not possible fro not logged user', async ({browser})=>{
+    const context = await browser.newContext({ storageState: undefined });// no storageState here
+    const page = await context.newPage();
+    const pm = new PageManager(page)
+    await page.goto('/');
+    await pm.onFeedPage().postFeedTitle("Post title 1").click()
+    await expect(pm.onPostPage().postAuthor).toHaveText(TESTUSER_1_NAME)
+    await pm.onPostPage().addComment(TEST_COMMENT1)
+    await expect(page.locator('h2')).toHaveText('Вхід в акаунт')
+})
+
 
 // test('edit own comment', async ({ page }) => {
 // });
